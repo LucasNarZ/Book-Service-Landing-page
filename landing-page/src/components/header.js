3e2b15f4f0e9fe61b10ad5e572a8e02f208c4cb4
LucasNarZ <lucasnarlochz@gmail.com>
2023-07-01 21:13:53 -0300

    header and hero responsive

--- landing-page/src/components/header.js
+++ landing-page/src/components/header.js
@@ -1,21 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/styles.css';
 
 import  bookIcon  from '../icons/icon_book.png';
 
+import MenuIcon from '@mui/icons-material/Menu';
+
 export function Header(props){
+    const [width, setWidth] = useState(window.innerWidth);
+    const [colapseMenu, setColapseMenu] = useState(false)
+
+    const handleResize = () => {
+        setWidth(window.innerWidth);
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+    
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return(
         <React.Fragment>
             <header id='header'>
-                <img src={bookIcon} className='logo'/>
-                <nav className='nav-bar'>
+                {width > 600 && 
+                <React.Fragment>
+                    <img src={bookIcon} className='logo'/>
+                    <nav className='nav-bar'>
                     <ul className='nav-list'>
                         <li><a href='/'>Home</a></li>
                         <li><a href='/plans'>Plans</a></li>
-
                         <li><a href='/about-us'>About Us</a></li>
                     </ul>
-                </nav>
+                    </nav>
+                </React.Fragment>}
+                {width <= 600 && 
+                <React.Fragment>
+                    <MenuIcon fontSize="large" sx={{
+                        m:"10px",
+                        cursor:"pointer", 
+                    }} onClick={() => {
+                        setColapseMenu(!colapseMenu)
+                    }}/>    
+                </React.Fragment>}
+                {console.log(colapseMenu)}
+                {colapseMenu === true && 
+                <React.Fragment>
+                    <nav className='nav-bar-burger'>
+                    <ul className='nav-list'>
+                        <li><a href='/'>Home</a></li>
+                        <li><a href='/plans'>Plans</a></li>
+                        <li><a href='/about-us'>About Us</a></li>
+                    </ul>
+                    </nav>
+                </React.Fragment>}
                 <a href='/plans'><button className='subscribe-btn'>Subscribe</button></a>
             </header>
         </React.Fragment>
